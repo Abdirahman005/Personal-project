feeb14eb86a4ca91c9e95285c44f12f731d6703d
Abdirahman005 <abdirahman.abdullah@student.moringaschool.com>
2026-01-19 22:35:20 +0300

    updated file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styles/Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Abdirahman Muktar. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Fathi Mohamed (Queen Fathi). All rights reserved.</p>
     </footer>
   );
 }
